e6c4b74c35b7e32af4fe1645149c049247aedc42
devdw98 <devdw98@gmail.com>
2021-06-28 02:13:09 +0900

    set test ssl

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-
+import {TEST_DIR, TEST_URL} from './vars'
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(TEST_URL, express.static(TEST_DIR))
 
 app.get('/', (req, res) => {
   res.send('Hello. This is SSL TestServer');

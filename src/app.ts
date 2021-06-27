969e438643765a54773c325ab7408f3a1894aa78
dw <devdw98@gmail.com>
2021-06-27 18:54:03 +0900

    modify console log

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello. This is SSL TestServer');
 });
 
 export default app;

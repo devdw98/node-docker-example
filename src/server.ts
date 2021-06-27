8bbb7d5831a7c96d70f7be56cc2f46e6f038e602
dw <devdw98@gmail.com>
2021-06-27 18:47:34 +0900

    http server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import app from './app';
+import http from 'http';
 
-const server = app.listen(app.get('port'), () => {
-  console.log(`listening at http://localhost:%d`, app.get('port'));
+const server_http = http.createServer(app).listen(app.get('port'), () => {
+  console.log(`http listening at http://localhost:%d`, app.get('port'));
 });

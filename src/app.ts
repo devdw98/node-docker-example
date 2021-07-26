1df4e0929fc4ad1b946075a0ff6b32ce5754da2e
dw <devdw98@gmail.com>
2021-07-26 16:34:23 +0900

    restart

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import {TEST_DIR, TEST_URL} from './vars'
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.use(TEST_URL, express.static(TEST_DIR))
 
 app.get('/', (req, res) => {
-  res.send('Hello. This is SSL TestServer');
+  res.send('Hello. This is test server');
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,5 @@
-// import app from './app';
-// import http from 'http';
-
-// const server_http = http.createServer(app).listen(app.get('port'), () => {
-//   console.log(`http listening at http://localhost:%d`, app.get('port'));
-// });
-
-
 import app from './app';
-import https from 'https';
-import fs from 'fs';
 
-const sslOptions = {
-  ca: fs.readFileSync('ca체인 루트/CA파일.crt'),
-  key: fs.readFileSync('키 파일 루트/키파일.key'),
-  cert: fs.readFileSync('인증서 경로/인증서 이름.crt'),
-}
-
-https.createServer(sslOptions, app).listen(app.get('port'), () => {
-  console.log(`https listening at https://localhost:%d`, app.get('port'));
+app.listen(app.get('port'), () => {
+  console.log(`http://localhost:%d`, app.get('port'));
 });

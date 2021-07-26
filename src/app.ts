775119b439a141fe79e4fa34d009af0eaaf29203
dw <devdw98@gmail.com>
2021-07-27 01:56:16 +0900

    set uuid

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,13 @@
 import express from 'express';
 import { v4 as uuid4 } from 'uuid';
 
-const uuid = () => {
-  const tokens = uuid4().split('-');
-  return tokens[2] + tokens[1] + tokens[0] + tokens[3] + tokens[4];
-};
 const app = express();
 
+const appid = uuid4();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-  res.send(`Hello. This is test server. ${uuid}`);
+  res.send(`Hello. This is test server. ${appid}`);
 });
 
 export default app;
